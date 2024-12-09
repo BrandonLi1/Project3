95702df34912f137e312b6a4faf8a58678add30e
BrandonLi1 <brandonjl@nycstudents.net>
2024-12-09 11:00:00 -0500

    Update MainLogic.java

--- src/MainLogic.java
+++ src/MainLogic.java
@@ -16,11 +16,11 @@ public class MainLogic {
     public void start() {
         String RED = "\u001B[31m"; //https://www.tutorialspoint.com/how-to-print-colored-text-in-java-console
         String RESET = "\u001B[0m";
-        while (playing == true) { //initializes to true, checks if it's still true and lets you play a game if it is
+        while (playing == true) {
             System.out.print(RED + "DISCLAIMER WE DO NOT CONDONE CHILD GAMBLING" + RESET + "\n");
             System.out.println("WELCOME TO THE MILLIONAIRE ISLAND!!!");
             boolean goodresponse = true;
-            while (goodresponse == true) { //checks if response is valid and if it isn't, it asks again
+            while (goodresponse == true) {
                 System.out.println("What game do you wish to play? 1) BlackJack 2) Roulette 3) Slots 4) Spinner Wheel");
                 int choice = scanner.nextInt();
                 if (choice == 1) {
@@ -40,6 +40,7 @@ public class MainLogic {
                     System.out.println("INVALID RESPONSE");
                     goodresponse = true;
                 }
+                playing = false;
             }
         }
     }
@@ -48,7 +49,7 @@ public class MainLogic {
     private void end() {
         System.out.println();
         boolean goodresponse2 = true;
-        while(goodresponse2 == true){ //initialized to true and if false it lets you leave the casino at a 70% chance
+        while(goodresponse2 == true){
             System.out.print("Do you wish to stay at the casino (yes /  no): ");
             String Stay = scanner.nextLine();
             if(Stay.equals("yes")) {
@@ -57,7 +58,7 @@ public class MainLogic {
                 start();
             } else if(Stay.equals("no")){
                 int addiction = (int)((Math.random()* 10) + 1);
-                if (addiction == 1 || addiction == 2 || addiction == 3) { //after this is run, because it's still in the "start" loop, it runs start again
+                if (addiction == 1 || addiction == 2 || addiction == 3) {
                     playing = true;
                     goodresponse2 = false;
                     System.out.println("do you know 90% of gamblers quit before they hit the jackpot???? \n ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
@@ -92,22 +93,26 @@ public class MainLogic {
         System.out.print("Welcome to BlackJack, how much do you want to bet: ");
         String playAgain = "";
         int blackbet = scanner.nextInt();
+        while (blackbet<=0) {
+            System.out.print("Invalid bet. bet again: ");
+            blackbet=scanner.nextInt();
+        }
         BlackJack blackjack = new BlackJack(cash, blackbet);
         BlackJack dealer = new BlackJack(cash);
         blackjack.BlackJackStart();
+    
         System.out.println("The dealer gives you your cards");
         System.out.println("The total value of your cards right now is " + blackjack.getHandValue());
+        dealer.BlackJackStart();
+        int dealerhand = dealer.getHandValue();
+        System.out.println("The dealer's current hand value is within the range of " + (dealer.getHandValue()-(int)(Math.random()*4+1)) + " to " + (dealer.getHandValue()+((int)(Math.random()*4)+1)));
         System.out.print("Do you wish to hit or stay? ");
         scanner.nextLine();
         String hitstay = scanner.nextLine();
         Boolean decisions = true;
-        dealer.BlackJackStart();
-        while(dealer.getHandValue() < 15 && blackjack.getHandValue()<21) {
-            dealer.hit();
-        }
         while (hitstay.equals("hit")) {
             blackjack.hit();
-            if (blackjack.gameOver()) {
+            if (blackjack.gameOver() == true) {
                 System.out.println("your new total value of your cards is " + blackjack.getHandValue());
                 if (blackjack.getHandValue() == 21) {
                     System.out.println("you won congrats");
@@ -122,23 +127,26 @@ public class MainLogic {
                 hitstay = scanner.nextLine();
             }
         }
+        while(dealer.getHandValue() < 15 && blackjack.getHandValue()<21) {
+            dealer.hit();
+        }
         System.out.println("Your hand value right now is " + blackjack.getHandValue());
         System.out.println("The dealers hand is " + dealer.getHandValue());
-        if (dealer.getHandValue() > 0 && dealer.getHandValue() <= 21) { //checks if dealer hand is within range
-            if(dealer.getHandValue() > blackjack.getHandValue() || blackjack.getHandValue()>21){ //checks for player loss
+        if (dealer.getHandValue() > 0 && dealer.getHandValue() <= 21) {
+            if(dealer.getHandValue() > blackjack.getHandValue() || blackjack.getHandValue()>21){
                 System.out.println("you lose");
                 this.cash -= blackbet;
-            } else if(dealer.getHandValue() < blackjack.getHandValue()){ //checks player win
+            } else if(dealer.getHandValue() < blackjack.getHandValue()){
                 System.out.println("you win");
                 this.cash += blackbet;
-            } else { //runs if equal hand value
+            } else {
                 System.out.println("It is a tie");
             }
         } else {
-            if(dealer.getHandValue() > 21 && blackjack.getHandValue() > 21 ){ //if dealer hand out of bounds but player hand also out
+            if(dealer.getHandValue() > 21 && blackjack.getHandValue() > 21 ){
                 System.out.println("The dealer also exceeded 21 but since you did first you lost");
                 this.cash -= blackbet;
-            } else { //only dealer out of bounds
+            } else {
                 System.out.println("you win");
                 this.cash+=blackbet;
             }
@@ -165,14 +173,34 @@ public class MainLogic {
         String x="";
         System.out.print("How much money do you want to bet on white(2x)? ");
         white=scanner.nextInt();
+        while (white<0) {
+            System.out.print("Invalid bet. bet again: ");
+            blackbet=scanner.nextInt();
+        }
         System.out.print("How much money do you want to bet on red(4x)? ");
         red=scanner.nextInt();
+        while (red<0) {
+            System.out.print("Invalid bet. bet again: ");
+            blackbet=scanner.nextInt();
+        }
         System.out.print("How much money do you want to bet on black(6x)? ");
         black=scanner.nextInt();
+        while (black<0) {
+            System.out.print("Invalid bet. bet again: ");
+            blackbet=scanner.nextInt();
+        }
         System.out.print("How much money do you want to bet on green(8x)? ");
         green=scanner.nextInt();
+        while (green<0) {
+            System.out.print("Invalid bet. bet again: ");
+            blackbet=scanner.nextInt();
+        }
         System.out.print("How much money do you want to bet on blue(21x)? ");
         blue=scanner.nextInt();
+        while (blue<0) {
+            System.out.print("Invalid bet. bet again: ");
+            blackbet=scanner.nextInt();
+        }
         spinner.spin(white, red, black, green, blue);
 
         x="y";
@@ -186,14 +214,34 @@ public class MainLogic {
                 if(x.equals("y")) {
                     System.out.print("How much money do you want to bet on white(2x)? ");
                     white=scanner.nextInt();
+                    while (white<0) {
+                        System.out.print("Invalid bet. bet again: ");
+                        blackbet=scanner.nextInt();
+                    }
                     System.out.print("How much money do you want to bet on red(4x)? ");
                     red=scanner.nextInt();
+                    while (red<0) {
+                        System.out.print("Invalid bet. bet again: ");
+                        blackbet=scanner.nextInt();
+                    }
                     System.out.print("How much money do you want to bet on black(6x)? ");
                     black=scanner.nextInt();
+                    while (black<0) {
+                        System.out.print("Invalid bet. bet again: ");
+                        blackbet=scanner.nextInt();
+                    }
                     System.out.print("How much money do you want to bet on green(8x)? ");
                     green=scanner.nextInt();
+                    while (green<0) {
+                        System.out.print("Invalid bet. bet again: ");
+                        blackbet=scanner.nextInt();
+                    }
                     System.out.print("How much money do you want to bet on blue(21x)? ");
                     blue=scanner.nextInt();
+                    while (blue<0) {
+                        System.out.print("Invalid bet. bet again: ");
+                        blackbet=scanner.nextInt();
+                    }
                 }
                 spinner.spin(white, red, black, green, blue);
             }
@@ -241,15 +289,19 @@ public class MainLogic {
         while(betgame.equals("y")) {
             System.out.println("How much do you want to bet: ");
             int roulettebet=scanner.nextInt();
+            while (roulettebet<0) {
+                System.out.print("Invalid bet. bet again: ");
+                blackbet=scanner.nextInt();
+            }
             scanner.nextLine();
             System.out.print("What color do you wish to bet on (red / black / none): ");
             String colorBet = scanner.nextLine();
-            if (!colorBet.equals("red") && !colorBet.equals("black")) { //checks if input is not equal to either of the accepted values
+            if (!colorBet.equals("red") && !colorBet.equals("black")) {
                 colorBet="";
             }
             System.out.print("Do you wish to bet on even or odd (even / odd / none): ");
             String evenBet = scanner.nextLine();
-            if (!evenBet.equals("odd") && !evenBet.equals("even")) { //checks if input is not equal to either of the accepted values
+            if (!evenBet.equals("odd") && !evenBet.equals("even")) {
                 evenBet="";
             }
             roulette.normalRoulette(roulettebet, colorBet,evenBet );
